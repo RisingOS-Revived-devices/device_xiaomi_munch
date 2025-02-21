49248ed935534ccd7f2264c78a424208ac20f396
Dhina17 <dhinalogu@gmail.com>
2025-02-21 07:29:22 +0000

    munch: Implement torch light control Change-Id: Icd32d1f6aedb55462c9df4d7cc63a2a4c4e4263e
    [RealJohnGalt:] modify for oneplus usage, add toggle switch
    [cyberknight777]: Adapt nodes and SEPolicy for sunny
    [Sanjivns]: Adapt LED nodes for alioth
    [SenX]: Adapt for Xiamoi sm8250
    
    Change-Id: I0b86fbeefbe3b951c5b9827a06c066dd71ada002
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    Signed-off-by: Sanjivns <alshahab109@gmail.com>
    Signed-off-by: SenseiiX <senseixdev@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -71,6 +71,9 @@ TARGET_NO_BOOTLOADER := true
 # Camera Malloc
 MALLOC_SVELTE_FOR_LIBC32 := true
 
+# Camera - Torch
+TARGET_CAMERA_SERVICE_EXT_LIB := //$(COMMON_PATH):libcameraservice_extension.xiaomi_sm8250
+
 # Display
 BOARD_USES_ADRENO := true
 TARGET_SCREEN_DENSITY := 440
