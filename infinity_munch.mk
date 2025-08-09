996f4a8b523cc261ef1af85cb4f9b1d62e71f73f
SENX | センクス <senseixdev@gmail.com>
2025-08-09 18:09:50 +0800

    munch: @ProjectInfinity-X
    Signed-off-by: SenX <senseiix@dev.com>

--- infinity_munch.mk
+++ infinity_munch.mk
@@ -8,17 +8,24 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common infinity stuff.
+$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
 
 # Inherit from munch device
 $(call inherit-product, device/xiaomi/munch/device.mk)
 
-PRODUCT_NAME := lineage_munch
+PRODUCT_NAME := infinity_munch
 PRODUCT_DEVICE := munch
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Poco
 PRODUCT_MODEL := 22021211RG
+
+# Infinity-X 
+INFINITY_BUILD_TYPE := UNOFFICIAL
+INFINITY_MAINTAINER := SENX|センクス
+TARGET_SUPPORTS_BLUR := true
+TARGET_DISABLE_EPPE := true
+WITH_GAPPS := false
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
