76a195cb779cd1d2a5daf60cab355a1d0da33fc5
swiitchOFF <120115258+swiitchOFF@users.noreply.github.com>
2026-04-11 09:39:25 +0530

    munch: Move c2_manifest_vendor.xml to device tree
    Change-Id: I59ceeec26346c0a04a636fa840218dd75b703b7b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -174,7 +174,8 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     $(DEVICE_PATH)/configs/hidl/framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml
-DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/hidl/manifest.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/hidl/manifest.xml \
+                        $(DEVICE_PATH)/configs/hidl/c2_manifest_vendor.xml
 DEVICE_MATRIX_FILE += hardware/qcom-caf/common/compatibility_matrix.xml
 ODM_MANIFEST_SKUS += nfc
 ODM_MANIFEST_NFC_FILES := $(DEVICE_PATH)/configs/hidl/manifest_nfc.xml
