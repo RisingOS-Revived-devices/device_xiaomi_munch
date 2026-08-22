9aef248c093bfddc7f4e45222a9bfcd148002a2d
efeisot <efeproofkmr@gmail.com>
2026-08-22 09:15:28 +0300

    Added flags for build (GMS)

--- lineage_munch.mk
+++ lineage_munch.mk
@@ -25,3 +25,12 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="munch-user 13 RKQ1.211001.001 V816.0.12.0.ULMMIXM release-keys" \
     BuildFingerprint=Redmi/munch/munch:13/RKQ1.211001.001/V816.0.12.0.ULMMIXM:user/release-keys
+
+RISING_MAINTAINER="SenX|センクス & efeisot"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    RisingChipset="Snapdragon 870" \
+    RisingMaintainer="SenX|センクス & efeisot"
+RISING_MAINTAINER := SenX|センクス & efeisot
+TARGET_ENABLE_BLUR := true
+PRODUCT_NO_CAMERA := false
+WITH_GMS := true
